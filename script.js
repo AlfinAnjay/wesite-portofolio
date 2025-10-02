4d999ec783eb58e9f6439e59f17c92cb7ea0d3b7
AlfinAnjay <proyekt4321@gmail.com>
2025-10-02 11:55:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Tombol Sapa
+const sapaButton = document.getElementById("sapaButton");
 
+sapaButton.addEventListener("click", () => {
+    alert("Halo! Terima kasih sudah mampir ke website saya 👋");
+    sapaButton.textContent = "😊 Sudah Disapa!";
+    sapaButton.disabled = true;
+
+    // Efek disabled style
+    sapaButton.style.background = "#ccc";
+    sapaButton.style.cursor = "not-allowed";
+});
